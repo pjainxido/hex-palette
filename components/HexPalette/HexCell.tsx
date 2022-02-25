8abfe5f8dcfa8d9a94bd99b62cf52248a7c03dcf
Park <pjane123@naver.com>
2022-02-26 00:30:20 +0900

    HexCell style Fix, add HexLabel on hover

--- components/HexPalette/HexCell.tsx
+++ components/HexPalette/HexCell.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
 interface HexCellProps {
@@ -5,45 +6,111 @@ interface HexCellProps {
 }
 
 const HexCell: React.FC<HexCellProps> = ({ hexCode }) => {
+  const [renderText, setRenderText] = useState<string>(hexCode);
+  const copiedText = 'copied!';
+
+  const alertOnTextCopied = () => {
+    setRenderText(copiedText);
+    setTimeout(() => {
+      setRenderText(hexCode);
+    }, 1000);
+  };
+
   const codeCopyToClipBoard = (code: string) => {
+    alertOnTextCopied();
     navigator.clipboard.writeText(code);
   };
   return (
-    <Cell
-      hexCode={hexCode}
-      onClick={() => codeCopyToClipBoard(`#${hexCode}`)}
-    />
+    <Cell hexCode={hexCode}>
+      <HexCodeText
+        onClick={() => codeCopyToClipBoard(hexCode)}
+        darken={renderText === copiedText}
+      >
+        {renderText}
+      </HexCodeText>
+    </Cell>
   );
 };
 
+interface IHexCodeText {
+  darken: boolean;
+}
+
+const HexCodeText = styled.span<IHexCodeText>`
+  position: absolute;
+  text-align: center;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 5rem;
+  color: #fff;
+  background-color: rgba(0, 0, 0, ${({ darken }) => (darken ? 1 : 0.1)});
+  font-size: 0.8rem;
+  opacity: 0;
+  padding: 0.5rem;
+  &:hover {
+    opacity: 1;
+  }
+`;
+
 const Cell = styled.div<HexCellProps>`
-  width: 100px;
-  height: 50px;
+  width: 7rem;
+  height: 3.5rem;
   position: relative;
   float: left;
-  background: ${({ hexCode }) => `#${hexCode}`};
-  margin-top: 30px;
-  margin-bottom: 5px;
-  margin-right: 5px;
+  background: ${({ hexCode }) => `${hexCode}`};
+  margin-top: 2.1rem;
+  margin-bottom: 0.35rem;
+  margin-right: 0.35rem;
   transition: all 0.3s ease;
   &:before {
     content: '';
     position: absolute;
-    border-bottom: 30px solid ${({ hexCode }) => `#${hexCode}`};
-    border-right: 50px solid transparent;
-    border-left: 50px solid transparent;
-    top: -30px;
+    border-bottom: 2.1rem solid ${({ hexCode }) => `${hexCode}`};
+    border-right: 3.5rem solid transparent;
+    border-left: 3.5rem solid transparent;
+    top: -2.1rem;
     transition: all 0.3s ease;
   }
   &:after {
     content: '';
     position: absolute;
-    border-top: 30px solid ${({ hexCode }) => `#${hexCode}`};
-    border-right: 50px solid transparent;
-    border-left: 50px solid transparent;
-    bottom: -30px;
+    border-top: 2.1rem solid ${({ hexCode }) => `${hexCode}`};
+    border-right: 3.5rem solid transparent;
+    border-left: 3.5rem solid transparent;
+    bottom: -2.1rem;
     transition: all 0.3s ease;
   }
 `;
+
+// const Cell = styled.div<HexCellProps>`
+//   width: 100px;
+//   height: 50px;
+//   position: relative;
+//   float: left;
+//   background: ${({ hexCode }) => `${hexCode}`};
+//   margin-top: 30px;
+//   margin-bottom: 5px;
+//   margin-right: 5px;
+//   transition: all 0.3s ease;
+//   &:before {
+//     content: '';
+//     position: absolute;
+//     border-bottom: 30px solid ${({ hexCode }) => `${hexCode}`};
+//     border-right: 50px solid transparent;
+//     border-left: 50px solid transparent;
+//     top: -30px;
+//     transition: all 0.3s ease;
+//   }
+//   &:after {
+//     content: '';
+//     position: absolute;
+//     border-top: 30px solid ${({ hexCode }) => `${hexCode}`};
+//     border-right: 50px solid transparent;
+//     border-left: 50px solid transparent;
+//     bottom: -30px;
+//     transition: all 0.3s ease;
+//   }
+// `;
 
 export default HexCell;
