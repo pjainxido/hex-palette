a303893a5f2806678ac80a8f381c946ae2fe40aa
Park <pjane123@naver.com>
2022-02-28 21:49:33 +0900

    Add palette Page

---
+++ pages/palette/index.tsx
@@ -0,0 +1,8 @@
+import { useRouter } from 'next/router';
+import { NextPage } from 'next';
+const PalettePage: NextPage = () => {
+  const router = useRouter();
+  return <div>index</div>;
+};
+
+export default PalettePage;
