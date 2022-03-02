3b95a474325d4e06a62f8f9915b416482046ce64
Park <pjane123@naver.com>
2022-03-02 22:30:31 +0900

    Update date string logic

--- utils/common.ts
+++ utils/common.ts
@@ -1,27 +1,30 @@
-const timeForToday = (value: Date, todayValue?: Date): string => {
+const getTimeDiff = (value: Date, todayValue?: Date): string => {
   const today = todayValue || new Date();
   const timeValue = new Date(value);
 
-  const betweenTime = Math.floor(
+  const timeDiff = Math.floor(
     (today.getTime() - timeValue.getTime()) / 1000 / 60
   );
-  if (betweenTime < 1) return '방금전';
-  if (betweenTime < 60) {
-    return `${betweenTime}분전`;
+  if (timeDiff < 1) return '방금전';
+  if (timeDiff < 60) {
+    return `${timeDiff}분전`;
   }
 
-  const betweenTimeHour = Math.floor(betweenTime / 60);
-  if (betweenTimeHour < 24) {
-    return `${betweenTimeHour}시간전`;
+  const hour = Math.floor(timeDiff / 60);
+  if (hour < 24) {
+    return `${hour}시간전`;
   }
 
-  const betweenTimeDay = Math.floor(betweenTime / 60 / 24);
-  if (betweenTimeDay < 30) {
-    return `${betweenTimeDay}일전`;
+  const days = Math.floor(hour / 24);
+  if (days < 7) {
+    return `${days}일전`;
   }
-  return `${value.getFullYear()}년 ${
-    value.getMonth() + 1
-  }월 ${value.getDate()}일`;
+  const weeks = days / 7;
+  if (weeks < 5) return `${Math.floor(weeks)}주 전`;
+  const months = days / 30;
+  if (months < 12) return `${Math.floor(months)}개월 전`;
+  const years = days / 365;
+  return `${Math.floor(years)}년 전`;
 };
 
-export { timeForToday };
+export { getTimeDiff };
