2b54c20db73d0e5dc1748d72af97b630d6b28c61
Park Jein <pjane123@naver.com>
2022-03-05 20:23:57 +0900

    Navigator (#6)
    * Add Navigator Links
    
    * Add Navigator Links

--- components/Navigator/index.tsx
+++ components/Navigator/index.tsx
@@ -1,7 +1,41 @@
+import Link from 'next/link';
 import styles from './Navigator.module.scss';
 
 const Navigator = () => {
-  return <nav className={styles.Navigator}>NAVIGATOR</nav>;
+  return (
+    <nav className={styles.Navigator}>
+      <div className={styles.pageLinks}>
+        <Link
+          href={{
+            pathname: '/',
+            query: { q: 'new' },
+          }}
+        >
+          <a>New</a>
+        </Link>
+        <Link
+          href={{
+            pathname: '/',
+            query: { q: 'hot' },
+          }}
+        >
+          <a>Hot</a>
+        </Link>
+        <Link
+          href={{
+            pathname: '/',
+            query: { q: 'random' },
+          }}
+        >
+          <a>Random</a>
+        </Link>
+        <Link href={'/like'}>
+          <a>Like</a>
+        </Link>
+      </div>
+      <div className={styles.tags}></div>
+    </nav>
+  );
 };
 
 export default Navigator;
