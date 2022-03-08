b84d92f6729fb22c8f1922aaef2107f7bf34b7a4
Park <pjane123@naver.com>
2022-03-08 22:00:12 +0900

    Add FilterContainer

---
+++ components/Header/FilterContainer/index.tsx
@@ -0,0 +1,5 @@
+const FilterContainer = () => {
+  return <div>Search</div>;
+};
+
+export default FilterContainer;
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,14 +1,21 @@
 import Link from 'next/link';
+import FilterContainer from './FilterContainer';
 import styles from './Header.module.scss';
 
 const Header = () => {
   return (
     <div className={styles.Header}>
-      <div className={styles.logo}>
-        <Link href={'/'}>
-          <a>HEX PALETTE</a>
-        </Link>
+      <div className={styles.left}>
+        <div className={styles.logo}>
+          <Link href={'/'}>
+            <a>HEX PALETTE</a>
+          </Link>
+        </div>
       </div>
+      <div className={styles.middle}>
+        <FilterContainer />
+      </div>
+      <div className={styles.right}>right</div>
     </div>
   );
 };
