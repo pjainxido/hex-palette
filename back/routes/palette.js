cc57e9ec94f7178a59a81d342b3e213e544420c7
Park <pjane123@naver.com>
2022-03-28 21:08:36 +0900

    Change Api Router Path

--- back/routes/palette.js
+++ back/routes/palette.js
@@ -3,22 +3,22 @@ const express = require("express");
 const Palette = require("../models/palette.model");
 const router = express.Router();
 
-router.get("/", (_, res) => {
-  // // mongoose.Collection('palette')
-  // Palette.find({}, (err, palettes)=>{
-  //   if(err) throw err;
-  //   console.log(palettes)
+// router.get("/", (_, res) => {
+//   // // mongoose.Collection('palette')
+//   // Palette.find({}, (err, palettes)=>{
+//   //   if(err) throw err;
+//   //   console.log(palettes)
 
-  // })
-  Palette.findAll()
-    .then((palette) => {
-      if (!palette.length) return res.status(404).send({ err: "Palette not found" });
-      res.send(palette);
-    })
-    .catch((err) => res.status(500).send(err));
-});
+//   // })
+//   Palette.findAll()
+//     .then((palette) => {
+//       if (!palette.length) return res.status(404).send({ err: "Palette not found" });
+//       res.send(palette);
+//     })
+//     .catch((err) => res.status(500).send(err));
+// });
 
-router.get("/filter", (req, res) => {
+router.get("/", (req, res) => {
   const { tags, sort, startDate, page = 0, limit = 50 } = req.query;
   const parsedTags = tags.split(",");
   Palette.findByFilterOptions(sort, parsedTags, startDate)
@@ -42,7 +42,7 @@ router.get("/id/:id", (req, res) => {
     });
 });
 
-router.post("/", (req, res) => {
+router.post("/create", (req, res) => {
   console.log(req.body);
   Palette.create(req.body)
     .then((palette) => res.send(palette))
