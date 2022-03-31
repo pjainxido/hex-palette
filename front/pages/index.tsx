a95f86670e9d6f674d0cfe014cb47698e052d86a
Park <pjane123@naver.com>
2022-03-31 23:15:08 +0900

    Update index.tsx

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,4 +1,5 @@
 import { GetServerSideProps } from 'next';
+import { useEffect, useState } from 'react';
 import type { NextPage } from 'next';
 
 import PaletteList, { IPaletteList } from 'components/PaletteList';
@@ -6,38 +7,51 @@ import { RootState } from 'store/modules';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
 import { checkDateOnRange } from 'utils/common';
+import { Palette } from 'store/modules/paletteList';
 
 const Home: NextPage<IPaletteList> = ({ contents }) => {
+  const [paletteList, setPaletteList] = useState<Palette[]>([]);
+  const [page, setPage] = useState<number>(1);
   const { tags, timeFrame, sort, title } = useSelector(
     (state: RootState) => state.filter
   );
 
-  const filterContents = contents
-    .filter((palette) => {
-      const isMatchTag = tags.every((tag) => palette.tags.includes(tag));
-      const isInTime = checkDateOnRange(palette.createdAt, timeFrame);
-      const isTitleMatch = palette.title.match(title);
-      return isMatchTag && isTitleMatch && isInTime;
-    })
-    .sort((prev, next) => {
-      const prevDate = new Date(prev.createdAt).getTime();
-      const nextDate = new Date(next.createdAt).getTime();
-      switch (sort) {
-        case 'hot':
-          return prev.like - next.like;
-        case 'old':
-          return prevDate - nextDate;
-        case 'random':
-          return Math.random() - 0.5;
-        case 'new':
-          return nextDate - prevDate;
-        default:
-          return nextDate - prevDate;
-      }
-    });
+  const filterPalette = (palette: Palette) => {
+    const isMatchTag = tags.every((tag) => palette.tags.includes(tag));
+    const isInTime = checkDateOnRange(palette.createdAt, timeFrame);
+    const isTitleMatch = palette.title.match(title);
+    return isMatchTag && isTitleMatch && isInTime;
+  };
+
+  const sortPalette = (prev: Palette, next: Palette) => {
+    const prevDate = new Date(prev.createdAt).getTime();
+    const nextDate = new Date(next.createdAt).getTime();
+    switch (sort) {
+      case 'hot':
+        return prev.like - next.like;
+      case 'old':
+        return prevDate - nextDate;
+      case 'random':
+        return Math.random() - 0.5;
+      case 'new':
+        return nextDate - prevDate;
+      default:
+        return nextDate - prevDate;
+    }
+  };
+
+  useEffect(() => {
+    const filterContents = contents
+      .filter((palette) => filterPalette(palette))
+      .sort((prev, next) => sortPalette(prev, next));
+    setPaletteList([...filterContents]);
+  }, []);
+
+  // useEffect(() => {}, [page]);
+
   return (
     <div>
-      <PaletteList contents={filterContents} />
+      <PaletteList contents={paletteList} />
     </div>
   );
 };
